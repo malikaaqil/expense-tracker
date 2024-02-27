ec656e88396b1d0d83a8b086d7c29640c32168ad
Malik Aaqil <malikaaqil2000@gmail.com>
2024-02-27 10:51:09 +0500

    updated app.js

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+
 let budgetInput = document.getElementById("budget-input");
 let setBudgetBtn = document.getElementById("set-input");
 let setDisplay = document.getElementById("set-display");
+let remDisplay = document.getElementById("rem-display");
 
 setBudgetBtn.addEventListener("click", () => {
-    // console.log(budgetInput.value);
-    setDisplay.innerHTML = "$: " + budgetInput.value
-    budgetInput.value = ""
+
+    if (setDisplay.innerHTML = budgetInput.value <= 0) {
+        Swal.fire("Your budget must be greater than zero.");
+
+    } else {
+        setDisplay.innerHTML = budgetInput.value
+        remDisplay.innerHTML = budgetInput.value
+        budgetInput.value = ""
+    }
+
+
 })
