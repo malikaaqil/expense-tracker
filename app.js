60ebe2995ed402de97c8f837a05d708ebd32940d
Malik Aaqil <malikaaqil2000@gmail.com>
2024-02-29 14:54:43 +0500

    updated app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ setBudgetBtn.addEventListener("click", () => {
 
     if (setDisplay.innerHTML = budgetInput.value <= 0) {
 
+        // custom alert from sweetalert2 library
+
         Swal.fire({
             icon: "error",
             title: "Your budget must be greater than zero."
@@ -22,4 +24,34 @@ setBudgetBtn.addEventListener("click", () => {
 
 })
 
+
+let expDescription = document.getElementById("exp-description");
+let expense = document.getElementById("expense");
+let date = document.getElementById("date");
+let addExpBtn = document.getElementById("add-expense");
+
+let tableList = document.getElementById("table-list");
+
+addExpBtn.addEventListener("click", () => {
+
+    // console.log(expDescription.value);
+    // console.log(expense.value);
+    // console.log(date.value);
+
+
+    tableList.appendChild = `<tr class="append-table"> <td>${expDescription.value}</td> <td>${expense.value}</td> <td>${date.value}</td> </tr>`
+
+
+    // console.log(appendTable);
+
+
+
+    remDisplay.innerHTML = +remDisplay.innerHTML - expense.value
+    expense.value = ""
+
+
+
+
+
+})
 
